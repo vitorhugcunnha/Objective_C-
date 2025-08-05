a7ccf6086338af0a3a148820e8af03c962b66c7e
vitorhugcunnha <vitor_hugo-cunha@estudante.sesisenai.org.br>
2025-08-04 22:02:46 -0300

    Comitando o Program OBJ

--- BoletimEscolar/Boletim.Console/Program.cs
+++ BoletimEscolar/Boletim.Console/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Quantidade de registros
+using System.Data;
+using System.Diagnostics;
+
+int QTD = 3;
+//Varoaveos de entrada
+int idade;
+string? nome;
+//vetores para amazenamento de regristros                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   '                                                                                                                                                                                                                                                                                               '                                                                                     
+int[] idades = new int[QTD];
+string[] nomes = new string[QTD];
+// Seçã de cadastro de alunos
+
+for (int i = 1; i <= QTD; i++)
+{
+    Console.Clear();
+    Console.WriteLine($"Cadastrando Registro {i} ");
+    //Console WriteLine é um Printf
+
+
+    Console.WriteLine("Nome: ");
+    nome = Console.ReadLine();
+    //ReadLine é um Scanf
+
+    Console.WriteLine("Idade: ");
+    idade = int.Parse(Console.ReadLine() ?? "0");
+
+    nomes[i] = nome ?? "0";
+    idades[i] = idade; 
+}
+
+
+//Seção de listagem
+Console.Clear();
+Console.WriteLine("Listagem de alunos");
+Console.WriteLine("-------------------");
+for (int i = 0; i < QTD; i++)
+{
+    Console.WriteLine($"{nomes[i]} \t {idades[i]}");
+}
+Console.WriteLine("Pressione uma tecla para finalizar.");
+Console.ReadKey();
